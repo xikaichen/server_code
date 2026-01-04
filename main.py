e901fab1066ed2ce37f893c9f1fa7c2d2d7d28ae
offer <offer@xikaideMac-mini.local>
2026-01-04 00:24:06 -0800

    reload set to false

--- main.py
+++ main.py
@@ -118,4 +118,4 @@ async def root():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True) 
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=False) 
